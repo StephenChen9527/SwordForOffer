6299f8575c2079b716fdc53801055350e22cdb83
StephenChen9527 <cundiandongxi1994@gmail.com>
2017-11-18 22:27:44 +0800

    初始化

---
+++ src/cn/edu/henu/subject1/Solution.java
@@ -0,0 +1,22 @@
+package cn.edu.henu.subject1;
+
+import org.junit.Test;
+
+public class Solution {
+	 public boolean Find(int target, int [][] array) {
+		for(int i=0;i<array.length;i++){
+			if((i+1)<array.length){
+				
+			}
+			
+		}
+		return false;
+	 }
+	 
+	 
+	 @Test
+	 public void test(){
+		 
+		 
+	 }
+}
