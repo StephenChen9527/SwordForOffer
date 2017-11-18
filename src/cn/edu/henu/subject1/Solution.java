56e1819b9470adfec2f248b76c45963ebf0fcaa4
StephenChen9527 <cundiandongxi1994@gmail.com>
2017-11-18 23:54:39 +0800

    题目2

--- src/cn/edu/henu/subject1/Solution.java
+++ src/cn/edu/henu/subject1/Solution.java
@@ -1,7 +1,13 @@
 package cn.edu.henu.subject1;
 
-import org.junit.Test;
-
+/**
+ * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，
+ * 每一列都按照从上到下递增的顺序排序。
+ * 请完成一个函数，输入这样的一个二维数组和一个整数，
+ * 判断数组中是否含有该整数。
+ * @author StephenChen9527
+ *
+ */
 public class Solution {
     public boolean Find(int target, int [][] array) {
 		if(array==null|| array.length==0 ||array[0].length==0 ||target<array[0][0] || target>array[array.length-1][array[0].length-1]){
